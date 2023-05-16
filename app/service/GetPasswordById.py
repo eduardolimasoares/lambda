81596e0047f4d327c365f9c79b469e520854b616
eduardo <eduardo.soares@supplylabs.com.br>
2023-05-16 12:22:02 -0300

    resolvendo problemas com request e método get

--- app/service/GetPasswordById.py
+++ app/service/GetPasswordById.py
@@ -1,4 +1,5 @@
 import boto3
+import time
 from botocore.exceptions import ClientError
 
 class GetPasswordById:
@@ -9,11 +10,19 @@ class GetPasswordById:
     def get_password(self, id):
         try:
             response = self.table.get_item(Key={'id': id})
+
+            can_view = self.can_view(self, response.get('num_views'), response.get('max_views'), response.get('valid_until') )
+            
             password = response.get('password')
-            if password:
+            
+            if can_view:
                 return password
             else:
                 return None
         except ClientError as e:
             print(f"Erro ao buscar senha do email")
             return None
+        
+    def can_view(self, num_views, max_views, valid_until ):
+        return num_views < max_views and time.time() < valid_until
+    
